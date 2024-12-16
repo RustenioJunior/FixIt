1c55381797bace2973e749e3c22d2858e2d1d947
Rustênio Júnior <rusteniojuniordev@gmail.com>
2024-12-16 18:45:49 +0000

    retira lixo

--- Authentication/obj/Debug/net9.0/Authentication.AssemblyInfo.cs
+++ Authentication/obj/Debug/net9.0/Authentication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd3cd7aa4a6f7d6c781ebb0d55cc2e940995843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d052bd6609206c495d63c0d0cc73474fe078e515")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
